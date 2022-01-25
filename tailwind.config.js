76ddc83c5c9e94bef4dbf20bce5c4a5c6893ed8e
Ido Evergreen <dicksonevergreenido@gmail.com>
2022-01-25 01:04:00 +0100

    adding background image newsletter

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,17 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-
       margin: {
-        '-72': '-18rem',
-       } ,
+        "-72": "-18rem",
+      },
       colors: {
         primary: "#FB9333",
-        'basic-black':'#575757',
-        'hero-action':' #FFF0DC',
-        'black-text':' #828282',
-        'testimonial-bg' : '#FFF8ED',
-        'menu-color' : '#585858',
-        
-        
+        "basic-black": "#575757",
+        "hero-action": " #FFF0DC",
+        "black-text": " #828282",
+        "testimonial-bg": "#FFF8ED",
+        "menu-color": "#585858",
       },
-       
 
       fontFamily: {
         sansx: "Poppins",
@@ -26,6 +22,11 @@ module.exports = {
       backgroundImage: (theme) => ({
         "hero-pattern":
           "url('https://res.cloudinary.com/evergreenx/image/upload/v1626020949/Group_8438_vfwwdj.svg')",
+
+ 
+          "newsletter-bg":
+          "url('https://res.cloudinary.com/evergreenx/image/upload/v1626020949/Group_8438_vfwwdj.svg')",
+
       }),
     },
   },
